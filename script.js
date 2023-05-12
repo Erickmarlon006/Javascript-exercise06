fe8ac1124acb14f221a493470c919492977f22b9
Erickmarlon006 <erickmarlonborbadesenna@gmail.com>
2023-05-11 21:33:57 -0300

    Sorteio pronto

---
+++ script.js
@@ -0,0 +1,23 @@
+var listaDeNomes = [];
+var contadeiro = 0;
+
+while(true){
+    listaDeNomes[contadeiro] = new Array();
+    listaDeNomes[contadeiro]['nome'] = prompt("Insira um nome para o sorteio:");
+    console.log(listaDeNomes[contadeiro]['nome']);
+
+    if(!confirm("Deseja colocar mais pessoas no sorteio?")){
+        alert('vamos ao sorteio');
+        break;
+    
+    }
+    contadeiro++;
+    
+ }
+ var sorteado = listaDeNomes[Math.floor(Math.random() * listaDeNomes.length)]['nome'];
+ console.log(sorteado);
+ alert("E o sorteado/a foi: " + sorteado.toUpperCase());
+ 
+ 
+
+        
